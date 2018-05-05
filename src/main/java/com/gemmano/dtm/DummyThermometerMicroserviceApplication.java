5f2a7e27290d3f3f3e24535b99be309161896777
Georgios Emmanouil <geo.emm@hotmail.com>
2018-05-05 23:45:16 +0300

    Initial commit

---
+++ src/main/java/com/gemmano/dtm/DummyThermometerMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.gemmano.dtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyThermometerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyThermometerMicroserviceApplication.class, args);
+	}
+}
